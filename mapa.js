69a8be413a5a230dfc6d399f9fb106bc184e6b31
Carlos Linares <cilp2912@gmail.com>
2016-01-25 20:41:45 -0600

    Se pinta matrix y cuadricula

--- mapa.js
+++ mapa.js
@@ -46,6 +46,36 @@ Mapa.prototype.dibujarCuadricula = function(){
     this.contexto.stroke();
 };
 
+Mapa.prototype.dibujarCuadriculaRect = function(){
+    // Dibujaremos unicamente cuadrados con sus lados iguales a this.tamanoCelda
+    
+    for (var x = 0; x !== this.ancho; x += this.tamanoCelda){
+        // this.contexto.moveTo(0, i);
+        // this.contexto.lineTo(this.ancho, i);
+        
+        for (var y = 0; y !== this.alto; y += this.tamanoCelda){
+            this.contexto.fillStyle = this.colorCuadricula;
+            this.contexto.fillRect(x, y, this.tamanoCelda, this.tamanoCelda);
+            console.log("Cuadro");
+        }
+    }  
+};
+
 Mapa.prototype.limpiar = function(){
     this.contexto.clearRect(0, 0, this.ancho, this.alto);  
+};
+
+Mapa.prototype.dibujarLinea = function(x1, y1, x2, y2, colorLinea){
+    
+    // Reemplazamod los pasos moveTo y LineTo por una sola llamada
+    // Punto de origen (x1, y1)
+    // Punto de fin (x2, y2)
+    
+    // Si colorLinea no se encuentra establecido se toma el default de this.color
+    colorLinea = (colorLinea == null) ? this.colorCuadricula : colorLinea;
+    
+    this.contexto.moveTo(x1, y1);
+    this.contexto.lineTo(x2, y2);
+    this.contexto.strokeStyle = colorLinea;
+    this.contexto.stroke();
 };
