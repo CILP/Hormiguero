47df73de633150f06913272f83b5d0c2d56dbe84
Carlos Linares <cilp2912@gmail.com>
2016-03-12 16:29:17 -0600

    Se agrega nueva capa y se dibuja la posicion de la hormiga en rojo

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // app.js
 // Carlos Linares
 var fondo,
+    media,
     frente,
     hormiga,
+    cabezaHormiga,
     dimensionHormiga;
 
 function repaint(){
   window.requestAnimationFrame(repaint);
+
   hormiga.mover(fondo.contexto);
+  
+  cabezaHormiga.x = hormiga.x;
+  cabezaHormiga.y = hormiga.y;
+  cabezaHormiga.pintar(media.contexto, "#F00", true);
 }
 
 window.addEventListener('load', function(){
@@ -18,13 +25,16 @@ window.addEventListener('load', function(){
   fondo = new Mapa(document.getElementById('fondo'), '2d', null, dimensionHormiga);
   frente = new Mapa(document.getElementById('frente'), '2d', null, dimensionHormiga);
 
-  // configCanvasListener();
+  // Se inicia la capa media
+  // donde se muestra el color de la hormiga
+  media = new Mapa(document.getElementById('media'), '2d', null, dimensionHormiga);
 
   frente.dibujarCuadricula();
   fondo.contexto.fillStyle = "#000";
   fondo.contexto.fillRect(0, 0, fondo.ancho, fondo.alto);
 
   hormiga = new Hormiga('Alfa', frente.ancho / 2, frente.alto / 2, dimensionHormiga, dimensionHormiga);
+  cabezaHormiga = new Hormiga('HeadAlfa', media.ancho / 2, media.alto / 2, dimensionHormiga, dimensionHormiga);
 
   repaint();
 }, false);
